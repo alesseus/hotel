49c919ae000a21052ef35395c4db069e70ba6d97
keanprivado5-lgtm <keanprivado5@gmail.com>
2026-07-17 11:02:43 +0200

    Stanze SPRING BOOT
    Creato il Model, Repository, Services, Controller di Stanza

--- backend/src/main/java/com/geak/hotel/Controller/StanzaController.java
+++ backend/src/main/java/com/geak/hotel/Controller/StanzaController.java
@@ -1,5 +1,43 @@
 package com.geak.hotel.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.geak.hotel.Model.Stanza;
+import com.geak.hotel.Services.StanzeSrv;
+
+@RestController
+@RequestMapping("/stanza/")
 public class StanzaController {
+	
+	@Autowired 
+	 private StanzeSrv camera;
+	
+	@GetMapping("elenco")
+	public List<Stanza> elencoStanze(){
+		return camera.getAllStanza();
+	}
+	
+	@GetMapping("elenco/{IDSTANZA}")
+	public Stanza tornaStanza(@PathVariable Long IDSTANZA) {
+		List<Stanza> tutte = camera.getAllStanza();
+		return tutte.stream().filter(
+				stanzaLetto -> stanzaLetto.getIDSTANZA() == IDSTANZA).findFirst().get();
+	}
+	
+	// Inserimento nuova stanza
+	@PostMapping("elenco")
+	public List<Stanza> inserisci(@RequestBody Stanza nuova){
+		camera.addStanza(nuova);
+		return camera.getAllStanza();
+	}
+	
 
 }
--- backend/src/main/java/com/geak/hotel/Services/StanzeSrv.java
+++ backend/src/main/java/com/geak/hotel/Services/StanzeSrv.java
@@ -1,5 +1,30 @@
 package com.geak.hotel.Services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.geak.hotel.Model.Stanza;
+import com.geak.hotel.Repository.StanzeRepo;
+
+@Service
 public class StanzeSrv {
+	@Autowired
+	private StanzeRepo stanzaRepo;
+	
+	public List<Stanza> getAllStanza(){
+		return stanzaRepo.findAll();
+	}
+	
+	public void addStanza(Stanza nuovaStanza) {
+		stanzaRepo.save(nuovaStanza);
+	}
+	
+	public Optional<Stanza> vediStanza(Long idVedi){
+		Optional<Stanza> stanzaV = stanzaRepo.findById(idVedi);
+		return stanzaV;
+	}
 
 }
