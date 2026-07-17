5758d468b89405f9111650a9f7d4ca870ad70c69
CartMaden <emanuelecampus11@gmail.com>
2026-07-17 12:01:56 +0200

    costruttore recensione

--- backend/src/main/java/com/geak/hotel/Model/Recensione.java
+++ backend/src/main/java/com/geak/hotel/Model/Recensione.java
@@ -7,41 +7,56 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="RECENSIONI")
+@Table(name = "RECENSIONI")
 public class Recensione {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long IDRECE;
 	private String DESCRIZIONE;
 	private int RATING;
 	private int IDCLIENTE;
-	
+
+	public Recensione() {
+	}
+
+	public Recensione(long IDRECE, String DESCRIZIONE, int RATING, int IDCLIENTE) {
+		setIDRECE(IDRECE);
+		setDESCRIZIONE(DESCRIZIONE);
+		setRATING(RATING);
+		setIDCLIENTE(IDCLIENTE);
+	}
+
 	public long getIDRECE() {
 		return IDRECE;
 	}
+
 	public void setIDRECE(long iDRECE) {
 		IDRECE = iDRECE;
 	}
+
 	public String getDESCRIZIONE() {
 		return DESCRIZIONE;
 	}
+
 	public void setDESCRIZIONE(String dESCRIZIONE) {
 		DESCRIZIONE = dESCRIZIONE;
 	}
+
 	public int getRATING() {
 		return RATING;
 	}
+
 	public void setRATING(int rATING) {
 		RATING = rATING;
 	}
+
 	public int getIDCLIENTE() {
 		return IDCLIENTE;
 	}
+
 	public void setIDCLIENTE(int iDCLIENTE) {
 		IDCLIENTE = iDCLIENTE;
 	}
-	
-	
 
 }
