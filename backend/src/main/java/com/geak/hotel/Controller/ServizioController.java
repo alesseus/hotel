422992c76191a94215bc7f662112c927b1dec370
keanprivado5-lgtm <keanprivado5@gmail.com>
2026-07-17 12:57:23 +0200

    SERVIZIO SPRING BOOT
    Fatto Controller, Responsive, Services

--- backend/src/main/java/com/geak/hotel/Controller/ServizioController.java
+++ backend/src/main/java/com/geak/hotel/Controller/ServizioController.java
@@ -1,5 +1,46 @@
 package com.geak.hotel.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.geak.hotel.Model.Servizio;
+import com.geak.hotel.Services.ServiziSrv;
+
+@RestController
+@RequestMapping("/servizio/")
 public class ServizioController {
+	@Autowired
+	private ServiziSrv serv;
+	
+	@GetMapping("elenco")
+	public List<Servizio> elencoServizio(){
+		return serv.getAllServizi();
+	}
+	
+	@GetMapping("elenco/{IDSERVIZIO}")
+	public Servizio tornaServ(@PathVariable Long IDSERVIZIO) {
+		List<Servizio> sv = serv.getAllServizi();
+		return sv.stream().filter(
+				servLetto -> servLetto.getIDSERVIZIO() == IDSERVIZIO).findFirst().get();
+	}
+	
+	// Aggiungo servizio
+	@PostMapping("elenco")
+	public ServiziSrv add(Servizio agg) {
+		serv.addServizio(agg);
+		return serv;
+	}
+	
+	@RequestMapping(value = "cancella/{IDSERVIZIO}", method = RequestMethod.DELETE)
+	public void cancella(@PathVariable Long idServCanc) {
+		serv.delServ(idServCanc);
+	}
 
 }
--- backend/src/main/java/com/geak/hotel/Services/ServiziSrv.java
+++ backend/src/main/java/com/geak/hotel/Services/ServiziSrv.java
@@ -1,5 +1,36 @@
 package com.geak.hotel.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.geak.hotel.Model.Servizio;
+import com.geak.hotel.Repository.ServiziRepo;
+
+@Service
 public class ServiziSrv {
+	@Autowired
+	private ServiziRepo servRepo;
+	
+	// Lista servizi
+	public List<Servizio> getAllServizi(){
+		return servRepo.findAll();
+	}
+	
+	// Aggiungi servizi
+	public void addServizio(Servizio nuovoServ) {
+		servRepo.save(nuovoServ);
+	}
+	
+	// Aggiorna servizio
+	public void aggServizio(Servizio aggServ) {
+		servRepo.save(aggServ);
+	}
+	
+	//Elimina servizio
+	public void delServ(Long IdCanc) {
+		servRepo.deleteById(IdCanc);
+	}
 
 }
