639a830aa09cbe92f41fe89efd79330dc0e5e626
Gabriele De Paolis <gdepaolis@studenti.laziodigital.it>
2026-07-20 15:40:41 +0200

    Gestione Servizi

--- frontend/src/app/gestisci-servizi/gestisci-servizi.ts
+++ frontend/src/app/gestisci-servizi/gestisci-servizi.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { servizio } from './interfacce/servizi_i';
 
 @Component({
   selector: 'app-gestisci-  servizi',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './gestisci-servizi.html',
   styleUrl: './gestisci-servizi.css',
 })
---
+++ frontend/src/app/gestisci-servizi/interfacce/servizi_i.ts
@@ -0,0 +1,5 @@
+export interface servizio {
+    "IDSERVIZIO": number
+    "NOTE" : string,
+    "COSTO": number
+}
