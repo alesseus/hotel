8fdba630442684a75d8463e73d8c28b995f175aa
keanprivado5-lgtm <keanprivado5@gmail.com>
2026-07-20 10:26:24 +0200

    creati login e registrazione
    component

---
+++ frontend/src/app/registrazione/registrazione.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrazione',
+  imports: [],
+  templateUrl: './registrazione.html',
+  styleUrl: './registrazione.css',
+})
+export class Registrazione {}
