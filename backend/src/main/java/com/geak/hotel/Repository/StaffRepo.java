a2ace1ee41ade3ddf957868f0328448f9e569d70
CartMaden <emanuelecampus11@gmail.com>
2026-07-20 17:03:44 +0200

    STAFF RIMOSSO E UNITO A CLIENTI
    MI VOGLIO UCCIDERE

--- backend/src/main/java/com/geak/hotel/Repository/StaffRepo.java
+++
@@ -1,6 +0,0 @@
-package com.geak.hotel.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.geak.hotel.Model.Staff;
-
-public interface StaffRepo extends JpaRepository<Staff, Long>{}
