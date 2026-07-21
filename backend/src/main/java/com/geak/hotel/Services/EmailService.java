f42dc2b2e2c1d81826336d92acda5602ded90d27
Gabriele De Paolis <gdepaolis@studenti.laziodigital.it>
2026-07-21 12:48:30 +0200

    fix vari ciao

--- backend/src/main/java/com/geak/hotel/Services/EmailService.java
+++ backend/src/main/java/com/geak/hotel/Services/EmailService.java
@@ -14,28 +14,30 @@ public class EmailService {
     private JavaMailSender mailSender;
 
     public void inviaConfermaPrenotazione(Prenotazione p) {
-        try {
-            SimpleMailMessage msg = new SimpleMailMessage();
-            msg.setFrom("nouseacc98@gmail.com");
-            msg.setTo(p.getEMAIL());
-            msg.setSubject("Conferma prenotazione");
-            msg.setText(
-                "Grazie per aver prenotato!\n\n" +
-                "Riepilogo prenotazione:\n" +
-                "━━━━━━━━━━━━━━━━━━━━━━\n" +
-                "Intestatario: " + p.getNOME() + " " + p.getCOGNOME() + "\n" +
-                "Email:        " + p.getEMAIL() + "\n" +
-                "Check-in:     " + p.getCHECK_IN() + "\n" +
-                "Check-out:    " + p.getCHECK_OUT() + "\n" +
-                "Totale:       €" + p.getTOTALE() + "\n" +
-                "Caparra:      €" + p.getCAPARRA() + "\n" +
-                "━━━━━━━━━━━━━━━━━━━━━━\n\n" +
-                "Per qualsiasi necessità contattaci.\n\n" +
-                "A presto!"
-            );
-            mailSender.send(msg);
-        } catch (Exception e) {
-            System.err.println("Errore invio mail: " + e.getMessage());
-        }
+        new Thread(() -> {
+            try {
+                SimpleMailMessage msg = new SimpleMailMessage();
+                msg.setFrom("nouseacc98@gmail.com");
+                msg.setTo(p.getEMAIL());
+                msg.setSubject("Conferma prenotazione");
+                msg.setText(
+                    "Grazie per aver prenotato!\n\n" +
+                    "Riepilogo prenotazione:\n" +
+                    "━━━━━━━━━━━━━━━━━━━━━━\n" +
+                    "Intestatario: " + p.getNOME() + " " + p.getCOGNOME() + "\n" +
+                    "Email:        " + p.getEMAIL() + "\n" +
+                    "Check-in:     " + p.getCHECK_IN() + "\n" +
+                    "Check-out:    " + p.getCHECK_OUT() + "\n" +
+                    "Totale:       €" + p.getTOTALE() + "\n" +
+                    "Caparra:      €" + p.getCAPARRA() + "\n" +
+                    "━━━━━━━━━━━━━━━━━━━━━━\n\n" +
+                    "Per qualsiasi necessità contattaci.\n\n" +
+                    "A presto!"
+                );
+                mailSender.send(msg);
+            } catch (Exception e) {
+                System.err.println("Errore invio mail: " + e.getMessage());
+            }
+        }).start();
     }
 }
