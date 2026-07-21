c47e18747718e00268983f732b14eb8513d7675b
Gabriele De Paolis <gdepaolis@studenti.laziodigital.it>
2026-07-21 10:00:20 +0200

    Aggiunta component pagamento

---
+++ frontend/src/app/pagamento/pagamento.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagamento',
+  imports: [],
+  templateUrl: './pagamento.html',
+  styleUrl: './pagamento.css',
+})
+export class Pagamento {}
