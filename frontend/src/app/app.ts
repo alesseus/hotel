aae4711d498cb040567971c09dfb17e99d2eef88
Gabriele De Paolis <gdepaolis@studenti.laziodigital.it>
2026-07-21 09:47:23 +0200

    Login/Logout navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from './login/Services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('hotel');
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
