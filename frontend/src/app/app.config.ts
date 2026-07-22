a4932de74276a3878def9af64f763d6f022113f0
Gabriele De Paolis <gdepaolis@studenti.laziodigital.it>
2026-07-22 10:35:53 +0200

    rollback prefix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { zoneInterceptor } from './zone.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' })
-    ),
-    provideHttpClient(withInterceptors([zoneInterceptor]))
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes,
+     withInMemoryScrolling({scrollPositionRestoration: 'top', anchorScrolling: 'enabled'})),
+     provideHttpClient()],
 };
